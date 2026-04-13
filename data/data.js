37cd381c836c7d8c46dc2a10d5c8b3a8ce5f70ca
bataraja646-del <bataraja646@gmail.com>
2026-04-13 21:12:25 +0700

    Create data/data.js

---
+++ data/data.js
@@ -0,0 +1,29 @@
+const sohibGuru = {
+  kelas: [
+    {
+      nama: "Kelas 4",
+      semester: [
+        {
+          nama: "Ganjil",
+          mapel: [
+            {
+              nama: "Matematika",
+              modul: [
+                {
+                  judul: "Sudut",
+                  materi: "<h3>Sudut</h3><p>Sudut adalah pertemuan dua garis.</p>",
+                  media: {
+                    gambar: [],
+                    video: [],
+                    ppt: []
+                  },
+                  soal: []
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
